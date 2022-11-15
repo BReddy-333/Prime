a997e2568d10d1c8bbcaba86185e0a8dcdaf4962
mannembhargavi <mannembhargavi@DESKTOP-RIB1OEG>
2022-11-15 13:18:49 +0530

    Second comitt

---
+++ src/promo/EvenDemo.java
@@ -0,0 +1,14 @@
+package promo;
+
+public class EvenDemo {
+	public static void main(String[] args) {
+		int n=7;
+		if(n/2==0) {
+			System.out.println("given number is even number");
+			}
+		else {
+			System.out.println("given number is not even number");
+		}
+
+}
+}
---
+++ src/promo/PrimeDemo.java
@@ -0,0 +1,16 @@
+package promo;
+
+public class PrimeDemo {
+	public static void main(String[] args) {
+		int num=3;
+for(int i=3;i<=num;i++) {
+if(num%i==0) {
+System.out.println("it is a prime number");
+		}
+else {
+	System.out.println("it is not a prime number");
+		
+	}
+
+}
+}}
